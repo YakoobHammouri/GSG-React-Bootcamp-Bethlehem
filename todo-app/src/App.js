31c3ec38e73e8549bfdd11c922e0b6ee88c5558a
YakoobHammouri <jakoub-h2002@hotmail.com>
2023-03-09 12:42:21 +0200

    todolist
    Signed-off-by: YakoobHammouri <jakoub-h2002@hotmail.com>

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -4,22 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div id="app">
-        <h1>Todo App</h1>
-        <div className="add-task">
-          <input type="text" id="task" placeholder="Add a new task" />
-          <button id="add-task">Add</button>
-        </div>
-        <ul id="task-list">
-          <li>
-            <div className="todo-item">
-              <span className=""> Test HTML Task</span>
-              <button className="btn-done">Done</button>
-              <button className="btn-delete">Delete</button>
-            </div>
-          </li>
-        </ul>
-      </div>
+     
     </div>
   );
 }
